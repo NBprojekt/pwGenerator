5e03d2e0af71c07d7311f36521821c7b3de04700
NBprojekt <bartko.norbert@gmail.com>
2017-08-10 12:58:38 +0200

    Add license header

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <!DOCTYPE html>
 <!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
+        pwGenerator generate rendom passwords
+
+    Copyright (C) 2017  NBprojekt
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 -->
 <html>
     <head>
--- print.php
+++ print.php
@@ -1,3 +1,16 @@
+<!--
+        pwGenerator generate rendom passwords
+
+    Copyright (C) 2017  NBprojekt
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+-->
 <?php 
 
 print $_POST['htmCode'];
